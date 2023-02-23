c953b57c9f8d0dd651a2f8f6e31cc9ea2a2bfe80
Dev1321 <125664041+Dev1321@users.noreply.github.com>
2023-02-23 16:38:22 +0500

    click manager script added

--- Assets/Firebase scripts/ClickManager.cs
+++ Assets/Firebase scripts/ClickManager.cs
@@ -1,14 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using System;
+
 
 public class ClickManager : MonoBehaviour
 {
-    
+    public Button Camera;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Camera.onClick.AddListener(Clicked); 
     }
 
+    private void Clicked()
+    {
+        Debug.Log(name + " button was clicked");
+    }
 }
