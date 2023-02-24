f06e887c365ff437b607a2302738df174319c3eb
Dev1321 <125664041+Dev1321@users.noreply.github.com>
2023-02-24 20:39:46 +0500

    UI manager script updated

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -7,7 +7,7 @@ public class UIManager : MonoBehaviour
 {
     //Buttons
     [SerializeField]
-    private Button HomeBottomPanelCamera;
+    private Button HomePanelToCameraPanel;
     [SerializeField]
     private Button HomeBottomPanelTrending;
     [SerializeField]
@@ -30,78 +30,74 @@ public class UIManager : MonoBehaviour
     private Button LanguageTopBarTick;
     //Panels
     [SerializeField]
-    private GameObject CameraPanel;
-    [SerializeField]
-    private GameObject HomePanel;
-    [SerializeField]
-    private GameObject GalleryPanel;
-    [SerializeField]
-    private GameObject SettingPanel;
-    [SerializeField]
-    private GameObject SettingLanguagePanel;
-    [SerializeField]
-    private GameObject CameraProPanel;
+    private GameObject CameraPanel, HomePanel, GalleryPanel, SettingPanel, SettingLanguagePanel, CameraProPanel;
 
-  
- 
+
     void Start()
     {
         HomePanel.SetActive(true);
-        HomeBottomPanelCamera.onClick.AddListener(CameraPanelActive);   
-        GalleryPanelHome.onClick.AddListener(HomePanelActive);
-        HomeBottomPanelGallery.onClick.AddListener(GalleryPanelActive);
-        HomeTopBarSetting.onClick.AddListener(SettingPanelActive);
-        BackCamera.onClick.AddListener(BackButton);
-        BackSetting.onClick.AddListener(BackButton);
-        SettingLanguage.onClick.AddListener(LanguagePanelActive);
-        LanguageTopBarTick.onClick.AddListener(HomePanelActive);
-        CameraPro.onClick.AddListener(CameraProPanelActive);
-        ProExit.onClick.AddListener(CameraPanelActive);
+        HomePanelToCameraPanel.onClick.AddListener(() => SwitchPanel(HomePanel, CameraPanel));   
+        GalleryPanelHome.onClick.AddListener(() => SwitchPanel(GalleryPanel, HomePanel));
+        HomeBottomPanelGallery.onClick.AddListener(() => SwitchPanel(HomePanel, GalleryPanel));
+        HomeTopBarSetting.onClick.AddListener(() => SwitchPanel(HomePanel, SettingPanel));
+        BackCamera.onClick.AddListener(() => SwitchPanel(CameraPanel, HomePanel));
+        BackSetting.onClick.AddListener(() => SwitchPanel(SettingPanel, HomePanel));
+        SettingLanguage.onClick.AddListener(() => SwitchPanel(SettingPanel, SettingLanguagePanel));
+        LanguageTopBarTick.onClick.AddListener(() => SwitchPanel(SettingLanguagePanel, HomePanel));
+        CameraPro.onClick.AddListener(() => SwitchPanel(CameraPanel, CameraProPanel));
+        ProExit.onClick.AddListener(() => SwitchPanel(CameraProPanel, CameraPanel));
     }
 
-    private void CameraPanelActive()
-    {
-        CameraPanel.SetActive(true);
-        HomePanel.SetActive(false);
-        CameraProPanel.SetActive(false);
-        Debug.Log("Camera button was clicked");
+    //private void CameraPanelActive()
+    //{
+    //    CameraPanel.SetActive(true);
+    //    HomePanel.SetActive(false);
+    //    CameraProPanel.SetActive(false);
+    //    Debug.Log("Camera button was clicked");
         
-        FirebaseManager.instance.LogEvent(name);
-    }  
-    private void CameraProPanelActive()
+    //    FirebaseManager.instance.LogEvent(name);
+    //}  
+    //private void CameraProPanelActive()
+    //{
+    //    CameraProPanel.SetActive(true);
+    //    CameraPanel.SetActive(false);
+    //}
+
+    //private void BackButton()
+    //{
+    //    HomePanel.SetActive(true);
+    //    CameraPanel.SetActive(false);
+    //    SettingPanel.SetActive(false);
+    //    Debug.Log("Back Button was clicked");
+    //}
+    //private void HomePanelActive()
+    //{
+    //    HomePanel.SetActive(true);
+    //    GalleryPanel.SetActive(false);
+    //    SettingLanguagePanel.SetActive(false);
+    //    Debug.Log("Gallery Home button was Clicked");
+    //}
+    //private void GalleryPanelActive()
+    //{
+    //    GalleryPanel.SetActive(true);
+    //    HomePanel.SetActive(false);
+    //}
+    //private void SettingPanelActive()
+    //{
+    //    SettingPanel.SetActive(true);
+    //    HomePanel.SetActive(false);
+    //}
+    //private void LanguagePanelActive()
+    //{
+    //    SettingLanguagePanel.SetActive(true);
+    //    SettingPanel.SetActive(false);
+    //}
+
+    private  void SwitchPanel(GameObject deactivate, GameObject activate)
     {
-        CameraProPanel.SetActive(true);
-        CameraPanel.SetActive(false);
-    }
-   
-  
-    private void BackButton()
-    {
-        HomePanel.SetActive(true);
-        CameraPanel.SetActive(false);
-        SettingPanel.SetActive(false);
-        Debug.Log("Back Button was clicked");
-    }
-    private void HomePanelActive()
-    {
-        HomePanel.SetActive(true);
-        GalleryPanel.SetActive(false);
-        SettingLanguagePanel.SetActive(false);
-        Debug.Log("Gallery Home button was Clicked");
-    }
-    private void GalleryPanelActive()
-    {
-        GalleryPanel.SetActive(true);
-        HomePanel.SetActive(false);
-    }
-    private void SettingPanelActive()
-    {
-        SettingPanel.SetActive(true);
-        HomePanel.SetActive(false);
-    }
-    private void LanguagePanelActive()
-    {
-        SettingLanguagePanel.SetActive(true);
-        SettingPanel.SetActive(false);
+        var currentPanel = deactivate;
+        currentPanel.SetActive(false);
+        var newPanel = activate;
+        newPanel.SetActive(true);
     }
 }
